12db29c606f6448cea6cf01de8520875dc9800d8
RaviTeja557-cloud <raviteja19991@gmail.com>
2024-12-19 11:29:48 +0530

    Create Band_name_Generator.py
    commit

---
+++ Band_name_Generator.py
@@ -0,0 +1,4 @@
+print("Welcome to the Band Name Generator Pro")
+City_name=input("Enter your city name?\n")
+Pet_Name=input("Enter your pet name?\n")
+print("your band name Could be :"+City_name+" "+Pet_Name)
